4131080a0cbf1b337c628312219dbf7c5119167a
Atom <atomcraft.4u@web.de>
2025-10-23 11:08:42 +0200

    madde

---
+++ 3/stochastik/ninjago.py
@@ -0,0 +1,18 @@
+import numpy
+import pandas
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import xticks
+
+ninjago = pandas.read_csv("./ninjagokarten.csv", sep=';')
+print(ninjago.head())
+ninjago = ninjago.drop('Name', axis=1).values.transpose()
+print(numpy.mean(ninjago, axis=1))
+print(numpy.std(ninjago, axis=1, ddof=1))
+
+color = (['yellow', 'red', 'blue', 'green'])
+farbe = (['gelb', 'rot', 'blau', 'grün'])
+#for i in range(0,4):
+    # plt.scatter(numpy.full((1,ninjago[i].size), i), ninjago[i], color=color[i])
+plt.boxplot(ninjago.transpose())
+xticks(range(1, 5), farbe)
+plt.show()
--- 3/stochastik/quantil.py
+++ 3/stochastik/quantil.py
@@ -1,9 +1,17 @@
 from statistics import quantiles
 import matplotlib.pyplot as plt
 import numpy
-x = numpy.array([14, 24, 22, 19, 18, 36, 15, 29, 41, 17])
+import scipy
+
+x = numpy.array([4.2, 3.9, 4.3, 4.1, 4.1, 3.7, 4.3])
+ad = numpy.array([9, 13, 15, 18, 20])
+gh = numpy.array([18, 37, 61, 125, 59])
+print(numpy.corrcoef(ad, gh))
+print(scipy.stats.linregress(ad, gh))
+print("-------")
 print(numpy.mean(x))
-print(numpy.quantile(x, [0.25, 0.5, 0.75, 0.9]))
+print(numpy.median(x))
+print(numpy.quantile(x, [0.25, 0.5, 0.75, 0.1]))
 print(numpy.std(x, ddof=1))
-plt.boxplot(x)
-plt.show()
+#plt.boxplot(x)
+#plt.show()
